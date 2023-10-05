bac03622be42a618ce3c036d8467ddbeae5fbefa
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-05 04:44:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", str);
+return (1);
+}
