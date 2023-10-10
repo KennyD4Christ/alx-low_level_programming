41253f9ec914e2be2ab37a4b9c8f458ea055438b
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-10 05:01:27 -0700

    files 2-print_alphabet_x10 and 3-islower.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int count;
+for (count = 0; count < 10; count++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+}
