6ce204f2ba6f53d7fbde919af109bcf5d26f70ba
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-10 09:42:38 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -20,6 +20,6 @@ sum += i; /* Add the multiple to the sum */
 }
 }
 /* Print the sum of multiples of 3 or 5 below 1024 */
-printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
+printf("%d\n", sum);
 return (0);
 }
