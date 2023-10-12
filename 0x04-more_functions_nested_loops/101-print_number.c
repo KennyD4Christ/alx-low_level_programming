c465e6ef84eff5b1a0f8550b68bed6ca6c6ed2f5
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-12 10:15:12 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,21 @@
 #include "main.h"
+
 /**
  * print_number - Prints an integer
  * @n: The integer to be printed
  */
 void print_number(int n)
 {
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-else if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-int divisor = 1;
-int temp = n;
-while (temp > 9)
-{
-temp /= 10;
-divisor *= 10;
-}
-while (divisor >= 1)
-{
-_putchar((n / divisor) +'0');
-n %= divisor;
-divisor /= 10;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
 }
