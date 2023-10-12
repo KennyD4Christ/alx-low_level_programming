7774799ba38692a0e0d7cd76d8ab7976a91b61a6
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-12 07:57:19 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - Entry point, Fizz-Buzz program
-*
-* Description:
-* This program prints numbers from 1 to 100 replace multiples of 3 with Fizz,
- * multiples of 5 with "Buzz," and multiples of both 3 and 5 with "FizzBuzz."
+ * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	/* Loop through numbers from 1 to 100 */
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
@@ -23,7 +20,6 @@ int main(void)
 		else
 			printf("%d", i);
 
-		/* Separate numbers/words with a space */
 		if (i < 100)
 			printf(" ");
 		else
