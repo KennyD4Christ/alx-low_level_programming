3ab80f6ec17041bc2320ebbf0e1d8bb41e2c8c28
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-16 05:03:12 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,9 @@ int i = 0;
 while (str[i] != '\0')
 {
 _putchar(str[i]);
-i += 2;  /* Increment the index by 2 to skip every other character */
+i++;
+if (str[i] == '\0')
+i++;
 }
 _putchar('\n');  /* Print a new line after the characters */
 }
