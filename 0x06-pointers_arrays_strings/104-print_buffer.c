d1be876f28e2648f75dc88ddc4e49da7dadfbc22
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-19 07:24:45 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -4,16 +4,6 @@
  * print_buffer - Prints the contents of a buffer in a specified format
  * @b: The buffer to be printed
  * @size: The size of the buffer
- *
- * Description: This function prints the content of size bytes of the buffer
- * pointed by b. The output should print 10 bytes per line, with each line
- * starting with the position of the first byte of the line in hexadecimal (8 chars).
- * Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes at a time,
- * separated by a space. Each line shows the content of the buffer. If the byte is a
- * printable character, it prints the letter; otherwise, it prints '.'.
- * Each line ends with a new line '\n'.
- *
- * If size is 0 or less, the output should be a new line only '\n'.
  */
 void print_buffer(char *b, int size) {
 	if (size <= 0) {
