843c064fec91af37e8832fce61a5db459ab53c25
KennyD4Christ <kennyd4christ@gmail.com>
2023-10-27 13:34:31 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Description: Prints the name of the program
- *
- * Return: Always 0
+ *  * main - Entry point
+ *   * @argc: The number of command-line arguments
+ *    * @argv: An array containing the command-line arguments
+ *     *
+ *      * Description: This program prints its name, followed by a new line.
+ *       * If you rename the program, it will print the new name
+ *       *without recompilation.
+ *        * It does not remove the path before the name of the program.
+ *         *
+ *          * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-printf("%s\n", __FILE__);
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
 return (0);
 }
