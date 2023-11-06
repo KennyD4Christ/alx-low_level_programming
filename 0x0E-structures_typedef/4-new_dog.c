121f397d3e2cbc988e0eff2f6ec6d7de001b456d
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-06 15:13:10 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,11 +16,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *new_dog;
 if (name == NULL || owner == NULL)
 return (NULL);
-dog_t *new_dog = malloc(sizeof(dog_t));
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
-return (NULL)
+return (NULL);
 new_dog->name = strdup(name);  /* Create a copy of the name */
 if (new_dog->name == NULL)
 {
