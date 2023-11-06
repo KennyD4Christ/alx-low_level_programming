0ede70e31d2ccaa32f9f4323f78b5775c44d3f84
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-06 12:01:50 +0100

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Prints the name of the source file
+ *     *
+ *      * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	    printf("This program was compiled from the file: %s\n", __FILE__);
+return (0);
+}
