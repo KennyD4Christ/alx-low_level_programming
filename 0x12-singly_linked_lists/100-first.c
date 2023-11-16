0a6ae68c245cec6b534461d8863c9f0a3e3eb009
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-16 12:31:00 +0100

    Updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,17 +8,3 @@ void pre_main_message(void)
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
-/**
- *  * main - Entry point of the program
- *   *
- *    * This function is the entry point of the program. It prints a message
- *     * before executing the rest of the main code.
- *      *
- *       * Return: Always 0 (Success)
- */
-int main(void)
-{
-pre_main_message(); /* Call the function before main */
-/* Rest of your main function */
-return (0);
-}
