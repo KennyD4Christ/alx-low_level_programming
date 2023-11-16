cd8272d886f0ab9b0c933dd406de0b42d43a1238
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-16 14:09:40 +0100

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,19 @@
 section .data
-hello db 'Hello, Holberton',0  ; null-terminated string
+    hello db 'Hello, Holberton', 0    ; null-terminated string
+    format db '%s', 0                 ; format string
 
 section .text
-global main
-extern printf
-main:
-mov rdi, hello      ; format string
-call printf         ; call printf function
+    global main
+    extern printf
 
-; Exit the program
-mov rax, 60         ; syscall: exit
-xor rdi, rdi        ; status: 0
-syscall
+main:
+    mov rdi, format     ; format specifier
+    mov rsi, hello      ; argument to print
+    xor rax, rax        ; clear any previous value in rax
+    call printf         ; call printf function
+
+    ; Exit the program
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; status: 0
+    syscall
+
