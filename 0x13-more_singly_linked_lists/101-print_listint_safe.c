a9f81417c49a9fe4ea437abfd1e79dac5dedaa6e
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-21 14:34:35 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,10 +9,11 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow, *fast;
+const listint_t *slow, *fast, *loop_start;
 size_t count = 0;
 slow = head;
 fast = head;
+loop_start = NULL;
 while (slow != NULL && fast != NULL && fast->next != NULL)
 {
 printf("[%p] %d\n", (void *)slow, slow->n);
@@ -21,6 +22,12 @@ fast = fast->next->next;
 count++;
 /* Check for a loop */
 if (slow == fast)
+{
+loop_start = slow;
+break;
+}
+}
+if (loop_start != NULL)
 {
 slow = head;
 while (slow != fast)
@@ -32,7 +39,6 @@ fast = fast->next;
 printf("-> [%p] %d\n", (void *)slow, slow->n);
 printf("Linked list has a loop\n");
 exit(98);
-}
 }
 return (count);
 }
