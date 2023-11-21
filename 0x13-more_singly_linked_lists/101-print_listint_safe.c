fc2505e34b4c848c381d20ed4a6ad6c7c3b22ec2
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-21 14:21:06 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,38 @@
 #include "lists.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 /**
  *  * print_listint_safe - prints a listint_t linked list safely
- *   * @head: pointer to the head of the linked list
- *    *
- *     * Return: the number of nodes in the list
+ *   * @head: pointer to the head of the list
+ *    * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *current = head;
-size_t node_count = 0;
-while (current != NULL)
+const listint_t *slow, *fast;
+size_t count = 0;
+slow = head;
+fast = head;
+while (slow != NULL && fast != NULL && fast->next != NULL)
 {
-/* Use printf to print to standard output */
-printf("[%p] %d\n", (void *)current, current->n);
-node_count++;
-/* Check for a loop by comparing addresses */
-if (current >= current->next)
+printf("[%p] %d\n", (void *)slow, slow->n);
+slow = slow->next;
+fast = fast->next->next;
+count++;
+/* Check for a loop */
+if (slow == fast)
 {
-/* Use write to print to standard error */
-write(2, "Linked list has a loop\n", 24);
+slow = head;
+while (slow != fast)
+{
+printf("[%p] %d\n", (void *)slow, slow->n);
+slow = slow->next;
+fast = fast->next;
+}
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+printf("Linked list has a loop\n");
 exit(98);
 }
-current = current->next;
 }
-return (node_count);
+return (count);
 }
