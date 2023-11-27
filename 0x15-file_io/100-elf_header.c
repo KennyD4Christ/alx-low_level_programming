cb56bcf4354279d84efbc6fa679a8a60236e2908
KennyD4Christ <kennyd4christ@gmail.com>
2023-11-27 19:21:10 +0100

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,31 +15,13 @@ dprintf(STDERR_FILENO, "%s\n", message);
 exit(98);
 }
 /**
- *  * print_magic - Prints the magic bytes from the ELF header.
- *   * @magic: Array containing the magic bytes.
- */
-void print_magic(unsigned char *magic)
-{
-printf("  Magic:   %02x %02x %02x %02x\n",
-magic[EI_MAG0], magic[EI_MAG1], magic[EI_MAG2], magic[EI_MAG3]);
-}
-/**
- *  * print_class - Prints the class of the ELF file.
- *   * @class: The class information from the ELF header.
- */
-void print_class(unsigned char class)
-{
-printf("  Class:                             %s\n",
-class == ELFCLASS32 ? "ELF32" :
-class == ELFCLASS64 ? "ELF64" : "Invalid class");
-}
-/**
  *  * read_elf_header - Reads and displays information from the ELF header.
  *   * @filename: The name of the ELF file.
  */
 void read_elf_header(const char *filename)
 {
-int fd, nread;
+int fd;
+ssize_t nread;
 Elf64_Ehdr elf_header;
 fd = open(filename, O_RDONLY);
 if (fd == -1)
@@ -55,11 +37,29 @@ elf_header.e_ident[EI_MAG3] != ELFMAG3)
 print_error("Error: Not an ELF file");
 }
 printf("ELF Header:\n");
-print_magic(elf_header.e_ident);
-print_class(elf_header.e_ident[EI_CLASS]);
+printf("  Magic:   %02x %02x %02x %02x\n",
+elf_header.e_ident[EI_MAG0], elf_header.e_ident[EI_MAG1],
+elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
+printf("  Class:                             %s\n",
+elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
+elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
+printf("  Data:                              %s\n",
+elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+elf_header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+printf("  Version:                           %d (current)\n",
+		elf_header.e_ident[EI_VERSION]);
+printf("  OS/ABI:                            %d\n",
+		elf_header.e_ident[EI_OSABI]);
+printf("  ABI Version:                       %d\n",
+		elf_header.e_ident[EI_ABIVERSION]);
+printf("  Type:                              %d\n",
+		elf_header.e_type);
+printf("  Entry point address:               0x%lx\n",
+		(unsigned long)elf_header.e_entry);
 close(fd);
 }
 #include "elf_header_reader.h"
+
 /**
  *  * main - Entry point for the ELF Header Reader program.
  *   * @argc: The number of command-line arguments.
