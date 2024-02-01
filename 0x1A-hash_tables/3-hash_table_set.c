c95d8341470714f67e0399ec1235683c12991d71
KennyD4Christ <kennyd4christ@gmail.com>
2024-02-01 14:27:10 +0100

    All adjusted

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,19 +14,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
 hash_node_t *new_node, *temp;
-
 if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 return (0);
-
 index = key_index((unsigned char *)key, ht->size);
-
-/* Check if the key already exists in the list at the index */
 temp = ht->array[index];
 while (temp != NULL)
 {
 if (strcmp(temp->key, key) == 0)
 {
-/* Update the value if the key already exists */
 free(temp->value);
 temp->value = strdup(value);
 if (temp->value == NULL)
@@ -35,19 +30,15 @@ return (1);
 }
 temp = temp->next;
 }
-
-/* If key doesn't exist, create a new node and add it to the list */
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 return (0);
-
 new_node->key = strdup(key);
 if (new_node->key == NULL)
 {
 free(new_node);
 return (0);
 }
-
 new_node->value = strdup(value);
 if (new_node->value == NULL)
 {
